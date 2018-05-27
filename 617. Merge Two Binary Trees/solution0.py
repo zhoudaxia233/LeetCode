5159e58515b3d89691cb9631bdb8ed51b95f2824
zheng <yootaoo@gmail.com>
2018-05-27 23:44:25 +0200

    first commit

---
+++ 617. Merge Two Binary Trees/solution0.py
@@ -0,0 +1,15 @@
+class Solution:
+    def mergeTrees(self, t1, t2):
+        """
+        :type t1: TreeNode
+        :type t2: TreeNode
+        :rtype: TreeNode
+        """
+        # This method also returns a new tree without changing the existing trees
+        if t1 and t2:
+            t = TreeNode(t1.val + t2.val)
+            t.left = self.mergeTrees(t1.left, t2.left)
+            t.right = self.mergeTrees(t1.right, t2.right)
+            return t
+        else:
+            return t1 or t2
---
+++ 617. Merge Two Binary Trees/solution1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def mergeTrees(self, t1, t2):
+        """
+        :type t1: TreeNode
+        :type t2: TreeNode
+        :rtype: TreeNode
+        """
+        # This method changes the existing tree instead of returning a new one
+        if t1 is None:
+            return t2
+        if t2 is None:
+            return t1
+        t1.val += t2.val
+        t1.left = self.mergeTrees(t1.left, t2.left)
+        t1.right = self.mergeTrees(t1.right, t2.right)
+        return t1

baf10206de06eb465bcc0df894c1bc2603857cff
zheng <yootaoo@gmail.com>
2018-05-29 01:15:33 +0200

    first commit

---
+++ 189. Rotate Array/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        k %= n
+        nums[:] = nums[n-k:] + nums[:n-k]

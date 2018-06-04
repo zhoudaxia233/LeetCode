d983cccaed1358662d64b1dd8133f45a851c0ff5
zheng <yootaoo@gmail.com>
2018-06-04 15:22:01 +0200

    first commit

---
+++ 121. Best Time to Buy and Sell Stock/solution.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        max_profit = 0
+        min_price = float('inf')
+        for price in prices:
+            if price < min_price:
+                min_price = price
+            profit = price - min_price
+            if profit > max_profit:
+                max_profit = profit
+        return max_profit

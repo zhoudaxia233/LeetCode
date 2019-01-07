7c58ece30fa59e3216a32985387604a612a682ef
zhoudaxia233 <yootaoo@gmail.com>
2019-01-07 11:44:39 +0100

    add 338 Counting Bits

---
+++ 338. Counting Bits/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def countBits(self, num):
+        """
+        :type num: int
+        :rtype: List[int]
+        """
+        res = [0]
+        for i in range(1, num + 1):
+            res.append(res[i >> 1] + (i % 2))
+        return res
